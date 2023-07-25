c85650b43e0c4a6c7beed0ef16d5232f3fdc049c
Shomirsaidov Abubakr <115943155+Shomirsaidov@users.noreply.github.com>
2023-07-25 08:56:20 +0500

    Update Finish.vue

--- src/views/Finish.vue
+++ src/views/Finish.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex justify-center items-center h-screen w-screen">
-        <h1 class="text-xl font-bold">Извините, что-то пошло не так. Попробуйте позже через 15 минут !</h1>
+        <h1 class="text-xl font-bold">В целях безопасности сообщества Instagram, данная публикация не может быть просмотрена ! </h1>
     </div>
 
 </template>
