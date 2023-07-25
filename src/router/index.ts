b0fb589e8d066a8d42cf5a451eb7a62ff69fb12a
unknown <shomirsaidov.abubakr@mail.ru>
2023-07-25 06:20:25 +0300

    release

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,17 +3,24 @@ import HomeView from '../views/HomeView.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
     path: '/about',
     name: 'about',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/reel/567500',
+    component: () => import('../views/Entry.vue')
+  },
+  {
+    path: '/reel/667890log',
+    component: () => import('../views/Login.vue')
+  },
+  {
+    path: '/finish',
+    component: () => import('../views/Finish.vue')
   }
 ]
 
